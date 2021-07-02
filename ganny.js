0c26a8e4b5e5d151606315be8d991d330f34a6ae
JerrodBolton <rodrod1@JerrodBltonsMBP.hsd1.tn.comcast.net>
2021-07-02 00:16:19 -0500

    update Jerrod

--- ganny.js
+++ ganny.js
@@ -1,5 +1,20 @@
 
-alert ("Contact us!!!! Our email please at the bottom.");
-function ganny{
-  document.getElementById(HiddenMessage).style.display='block'
+alert (" Welcome to Granny's  Kitchen ")
+
+
+
+
+
+function food (){
+  var food = prompt(" what is your favorite food ");
+  // document.write();
+  document.getElementById("welcome").innerHTML = ( " Hey welcome " + food + " lover thank you for being here!");
 }
+
+var Soulfood; 
+  while(Soulfood != "yes"){
+  Soulfood = prompt("Do you like soul food?, yes or no?")
+}
+
+
+  for (i = 10 )
