55b6f6fa731c8475cdd2cc3e8fc13e86620d1984
JerrodBolton <rodrod1@JerrodBltonsMBP.hsd1.tn.comcast.net>
2021-07-02 21:06:11 -0500

    test

--- ganny.js
+++ ganny.js
@@ -1,20 +1,29 @@
+alert(" Welcome to Granny's Kitchen ");
 
-alert (" Welcome to Granny's Kitchen ")
-
-
-
-
-
-function food (){
+function food() {
   var food = prompt(" what is your favorite food ");
   // document.write();
-  document.getElementById("welcome").innerHTML = ( " Hey welcome " + food + " lover thank you for being here!");
+  document.getElementById("welcome").innerHTML =
+    " Hey welcome " + food + " lover thank you for being here!";
 }
 
-var Soulfood; 
-  while(Soulfood != "yes"){
-  Soulfood = prompt("Do you like soul food?, yes or no?")
+var Soulfood;
+while (Soulfood != "yes") {
+  Soulfood = prompt("Do you like soul food?, yes or no?");
+}
+
+// What are dome ideas that you want to do with your javascript.
+
+function checkweight(){
+  let weight = prompt ("How much do you weight");
+  if ( weight > 150 ) {//then
+      
+    alert(" Your to fat to be here go away"); //then
+
+
+     
+  }
 }
 
 
-  for (i = 10 )
+checkweight();
