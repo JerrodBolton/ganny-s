1ec55d922127b749b1a4226c1fe9fbb6e299779f
JerrodBolton <rodrod1@JerrodBltonsMBP.hsd1.tn.comcast.net>
2021-07-04 23:12:31 -0500

    updating js

--- ganny.js
+++ ganny.js
@@ -1,16 +1,20 @@
  alert("Welcome to Granny's Kitchen");
 
-function food() {
-  var food = prompt("what is your favorite food ");
-  // document.write();
-  document.getElementById("welcome").innerHTML =
-   (" Hey welcome " + food + " lover thank you for being here!");
+function food(){
+  let your = prompt("what is your favorite food ");
+   document.write();
+    document.getElementById("welcome").innerHTML =
+   (" Hey, welcome " + your + " lover thank you for being here!");
 }
 
-var Soulfood;
+
+
+let Soulfood;
 while (Soulfood != "yes"){
   Soulfood = prompt("Do you like soul food?, yes or no?");
 }
+
+
 
 // What are dome ideas that you want to do with your javascript.
 /* The the idea that I haveI will to make a weight checker. This will check the weight of people that come to the website. 
@@ -19,20 +23,16 @@ less than that weight than they will get to be in the weight. Comments to add is
 need so good soul food come get you some to eat."
 */
 
-  Function checkweight(){
+function checkweight(){
   let weight = prompt("How much do you weight");
   if (weight > 170) { //then
-    alert(" Your to fat to be here go away"); 
-   
-  }
-  if( weight <= 170){ //then
+   alert(" Your to fat to be here go away"); 
 
+  }
+  if( weight < 170){ //then
    alert ("You need some meat on your bones. Get food now!!!!! But read my story first this took me a lot of time todo. Then go get something to eat. Now");
   }
-
 }
 
-checkweight(); /*
 
-
-
+checkweight();
